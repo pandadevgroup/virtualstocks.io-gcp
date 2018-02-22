dbc8b009ea4bf055827142c8242bec7da4c6e8f9
Nathan Wang <nathan.r.wang@gmail.com>
2018-02-21 16:56:44 -0800

    trade only during trading hours

--- build/util/stocks-watcher.js
+++ build/util/stocks-watcher.js
@@ -13,13 +13,22 @@ exports.StockChange = StockChange;
 class StocksWatcher {
     constructor() {
         this.callbacks = [];
-        this.socket = socketIOClient('https://ws-api.iextrading.com/1.0/last');
+        this.socketOpen = false;
+        this.socket = socketIOClient('https://ws-api.iextrading.com/1.0/last', {
+            autoConnect: false
+        });
         this.socket.on('message', message => this.handleMessage(JSON.parse(message)));
         // Connect to the channel
         this.socket.on('connect', () => {
             console.log("[Stocks Watcher] Connected to ws-api.iextrading.com.");
+            this.socketOpen = true;
         });
-        this.socket.on('disconnect', () => console.log('[Stocks Watcher] Disconnected from ws-api.iextrading.com.'));
+        this.socket.on('disconnect', () => {
+            this.socketOpen = false;
+            console.log('[Stocks Watcher] Disconnected from ws-api.iextrading.com.');
+        });
+        this.checkSocket();
+        setInterval(this.checkSocket.bind(this), 1000 * 60);
     }
     watch(ticker) {
         this.socket.emit("subscribe", ticker);
@@ -29,6 +38,22 @@ class StocksWatcher {
     }
     onChange(callback) {
         this.callbacks.push(callback);
+    }
+    checkSocket() {
+        const date = new Date();
+        const hour = date.getUTCHours() + 1;
+        const minutes = date.getUTCMinutes();
+        // Opens 2:30 PM, closes 9 PM
+        if (hour == 2) {
+            if (minutes >= 29 && !this.socketOpen) {
+                // Past 2:29, start trading
+                this.socket.open();
+            }
+        }
+        else if (hour == 9 && this.socketOpen) {
+            // It's 9, stop trading
+            this.socket.close();
+        }
     }
     handleMessage(message) {
         const { symbol: ticker, price, time } = message;
